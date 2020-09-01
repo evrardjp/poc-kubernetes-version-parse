8196b025a3f5be64df42d8791f35ecac28fab413
Jean-Philippe Evrard <jean-philippe@evrard.me>
2020-09-01 16:45:58 +0200

    Show errors if you do crap with consts

--- comparison.go
+++ comparison.go
@@ -14,11 +14,16 @@ const (
 func main() {
 	var baseVersion *version.Version
 	baseVersion, _ = version.ParseSemantic(StringOne)
-	cmpResult, _ := baseVersion.Compare(StringTwo)
+	cmpResult, err := baseVersion.Compare(StringTwo)
+	if err != nil {
+		fmt.Println(err)
+	}
 	switch {
 	case cmpResult == 1:
 		fmt.Printf("%v is newer than %v", StringOne, StringTwo)
 	case cmpResult == -1:
 		fmt.Printf("%v is older than %v", StringOne, StringTwo)
+	case cmpResult == 0:
+		fmt.Printf("Same version")
 	}
 }
